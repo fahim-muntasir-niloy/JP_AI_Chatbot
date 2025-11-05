95093ca23cd3695ad4aa0aeb453328c3b0a0e7d4
Fahim Muntasir <muntasirfahim.niloy@gmail.com>
2025-11-05 14:47:12 +0600

    Update environment variable for API key and clean up model initialization

--- models/llm.py
+++ models/llm.py
@@ -4,11 +4,10 @@ from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ["ANTHROPIC_API_KEY"] = os.getenv("ANTHROPIC_API_KEY")
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
-# llm = init_chat_model(
-#     model="gemini-2.5-flash", model_provider="google_genai", temperature=0.5
-# )
-
 llm = init_chat_model(model="qwen/qwen3-32b", model_provider="groq", temperature=0.5)
+# llm = init_chat_model(
+#     model="claude-haiku-4-5-20251001", model_provider="anthropic", temperature=0.5
+# )
